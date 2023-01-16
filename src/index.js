06acbeac46ce511d257394e9afe214c93ade4d86
Philipp Dobrikov <philipp.dobrikov@yandex.ru>
2023-01-16 14:25:14 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let i = 0;
+    let result = '';
+    let letter = {};
+    
+    while (i <= expr.length) {
+      if (i > 0 && i % 10 == 0) {
+        if (result == '**********'){
+          letter[i / 10] = ' ';
+        } else {
+            // letter[i / 10] = String(+result).replaceAll('10', '.').replaceAll('11', '-');
+            letter[i / 10] = String(+result).replace(/10/g, '.').replace(/11/g, '-');
+        }
+        result = '';
+      }
+      result = result + expr[i];
+      i++;
+    }
+    // console.table(letter);
+    result = '';
+    for (let key in letter) {
+      if (letter[key] == ' ') {
+        result = result + ' ';
+      } else {
+        result = result + MORSE_TABLE[letter[key]];
+      }
+    }
+    return result;
 }
 
 module.exports = {
